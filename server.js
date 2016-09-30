de71feef915167ddbfcc0b0443f8f8f971aaff46
senthileniyavan <senthileniyavan8@gmail.com>
2016-09-30 20:23:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article one', function (req, res) {
+app.get('/articleone', function (req, res) {
     res.send("article one requested and will be serverd");
 });
 app.get('/ui/style.css', function (req, res) {
